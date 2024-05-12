761ac9e8aa67a0f8e7c90ee99b201eacb67af968
Vinzent Brömauer <vinz.corno@web.de>
2024-05-12 02:05:01 +0200

    implement bool expression

--- AntlrLangDev/GScriptVisitor.cs
+++ AntlrLangDev/GScriptVisitor.cs
@@ -13,25 +13,30 @@ namespace AntlrLangDev
             string name = context.IDENTIFIER().GetText();
             object? value = Visit(context.expression());
             Variables[name] = value;
-            
+
             return null;
         }
 
         public override object VisitConstant([Antlr4.Runtime.Misc.NotNull] GScriptParser.ConstantContext context)
         {
-            if(context.INTEGER() is {} i){
+            if (context.INTEGER() is { } i)
+            {
                 return int.Parse(i.GetText());
             }
-            if(context.FLOAT() is {} f){
+            if (context.FLOAT() is { } f)
+            {
                 return float.Parse(f.GetText());
             }
-            if(context.STRING() is {} s){
+            if (context.STRING() is { } s)
+            {
                 return s.GetText()[1..^1];
             }
-            if(context.BOOL() is {} b){
+            if (context.BOOL() is { } b)
+            {
                 return bool.Parse(b.GetText());
             }
-            if(context.NULL() is {}){
+            if (context.NULL() is { })
+            {
                 return null;
             }
             throw new NotImplementedException();
@@ -40,7 +45,8 @@ namespace AntlrLangDev
         public override object VisitIdentifierExpression([NotNull] GScriptParser.IdentifierExpressionContext context)
         {
             var varname = context.IDENTIFIER().GetText();
-            if(!Variables.ContainsKey(varname)){
+            if (!Variables.ContainsKey(varname))
+            {
                 throw new Exception($"error, variable {varname} not found");
             }
             return Variables[varname];
@@ -48,27 +54,57 @@ namespace AntlrLangDev
 
         public override object VisitNegatedExpression([NotNull] GScriptParser.NegatedExpressionContext context)
         {
-            var varname = context.expression().GetText();
-            if(!Variables.ContainsKey(varname)){
-                throw new Exception($"error, variable {varname} not found");
+            var value = Visit(context.expression());
+
+            if (value.GetType() == typeof(bool))
+            {
+                return !(bool)value;
             }
-            var variable = Variables[varname];
-            if(variable.GetType() == typeof(bool)){
-                return !(bool)variable;
-            }
-            throw new Exception($"error, variable of type {variable.GetType()} cannot be negated.");
+            throw new Exception($"error, variable of type {value.GetType()} cannot be negated.");
         }
 
         public override object VisitMultExpression([NotNull] GScriptParser.MultExpressionContext context)
         {
             var expressions = context.expression();
+            var op = context.multOp();
+            
             bool isInt = true;
 
-            foreach(var expression in expressions){
+            foreach (var expression in expressions)
+            {
                 
             }
 
             return base.VisitMultExpression(context);
+        }
+
+        public override object VisitBoolExpression([NotNull] GScriptParser.BoolExpressionContext context)
+        {
+            var expressions = context.expression();
+            var op = context.boolOp();
+
+            var res1 = Visit(expressions[0]);
+            var res2 = Visit(expressions[1]);
+
+            if (res1.GetType() != typeof(bool) || res2.GetType() != typeof(bool))
+            {
+                throw new Exception("error, non-boolean value used for bool operator");
+            }
+
+            switch (op.GetText())
+            {
+                case "&":
+                    return (bool)res1 & (bool)res2;
+                case "|":
+                    return (bool)res1 | (bool)res2;
+            }
+
+            throw new Exception("error, invalid bool operation");
+        }
+
+        public override object VisitEnclosedExpression([NotNull] GScriptParser.EnclosedExpressionContext context)
+        {
+            return Visit(context.expression());
         }
     }
 }
