396d1bfc19357ad194745f2264e50c1fdd8e1ef7
Vinzent Brömauer <vinz.corno@web.de>
2024-05-14 11:21:01 +0200

    fix error line output

--- AntlrLangDev/GScriptErrorListener.cs
+++ AntlrLangDev/GScriptErrorListener.cs
@@ -7,7 +7,7 @@ namespace AntlrLangDev
         public void SyntaxError(TextWriter output, IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
         {
             //we immediately throw since continuing execution would immediately end in undefined behavior
-            throw new Exception($"parser encountered unexpected input at line {14}.");
+            throw new Exception($"parser encountered unexpected input at line {line}.");
         }
     }
 }
